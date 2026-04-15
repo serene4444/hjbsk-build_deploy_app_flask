70de1987aed1eeb4cf178a17ad51bb6cbf319ab4
Plummer, Serene <sereneplummer@my.unt.edu>
2026-04-15 17:16:25 -0500

    Implement arithmetic operations and update server routes for integer results

--- server.py
+++ server.py
@@ -1,29 +1,43 @@
+from Maths.mathematics import summation, subtraction, multiplication
 from flask import Flask, render_template, request
-# Import the Maths package here
 
 app = Flask("Mathematics Problem Solver")
 
+
 @app.route("/sum")
 def sum_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    # Write your code here
+    num1 = float(request.args.get("num1"))
+    num2 = float(request.args.get("num2"))
+    result = summation(num1, num2)
+    if result.is_integer():
+        result = int(result)
+    return str(result)
+
 
 @app.route("/sub")
 def sub_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    # Write your code here
+    num1 = float(request.args.get("num1"))
+    num2 = float(request.args.get("num2"))
+    result = subtraction(num1, num2)
+    if result.is_integer():
+        result = int(result)
+    return str(result)
+
 
 @app.route("/mul")
 def mul_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    # Write your code here  
+    num1 = float(request.args.get("num1"))
+    num2 = float(request.args.get("num2"))
+    result = multiplication(num1, num2)
+    if result.is_integer():
+        result = int(result)
+    return str(result)
+
 
 @app.route("/")
 def render_index_page():
-    # Write your code here
-    
+    return render_template("index.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
